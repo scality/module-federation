429530e9532cbf097ca6130b3b1356b1c61c564a
Jean-Marc Millet <jean-marc.millet@scality.com>
2026-01-14 11:43:45 +0100

    test with retry

--- src/ModuleFederation.tsx
+++ src/ModuleFederation.tsx
@@ -58,7 +58,22 @@ export function loadModule(
   return async () => {
     // Initializes the share scope. This fills it with known provided modules from this build and all remotes
     await __webpack_init_sharing__("default");
-    const container = window[scope]; // or get the container somewhere else
+
+    let container: any = window[scope];
+    const startTime = Date.now();
+    const timeout = 2000;
+    const interval = 100;
+
+    while (!container && Date.now() - startTime < timeout) {
+      await new Promise((resolve) => setTimeout(resolve, interval));
+      container = window[scope];
+    }
+
+    if (!container) {
+      throw new Error(
+        `Container for scope "${scope}" could not be found within timeout period.`
+      );
+    }
     // Initialize the container, it may provide shared modules
     await container.init(__webpack_share_scopes__.default);
     const factory = await window[scope].get(module);
